67a56682c621ab8c43ff1a2bc2395093c4a42dc7
pedrofbr0 <pedrofbr@hotmail.com>
2021-11-28 21:32:02 -0300

    Refactoring the App form functional to class component

--- src/index.js
+++ src/index.js
@@ -6,22 +6,42 @@ if(module.hot) {
   module.hot.accept();
 }
 
-const App = function() {
+// with the functional component, we don't need to use the render method
+// ReactDOM.render(<App />, document.querySelector('#root'));
 
-  window.navigator.geolocation.getCurrentPosition( //make the browser try to find geolocalization of user (sometimes asks the permission)
-    (position) => console.log(position), //sucess callback, it's called when the function finds a position
-    (err) => console.log(err) //failure callback, it's called when the function can't find the user's position
-  );
+// with the functional component, we don't have a good way to wait for
+// the responses from the callbacks to the getCurrentPosition function
+// Therefore, we need to use class componenets
 
-  return ( 
+// Declaring a class component: react expects a class component to have a render method
+// and a bunch of other methods. That's why we extend React.Component.
+
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      err => console.log(err)
+    );
+    return <div>Latitude:</div>;
+  }
+}
+
+// const App = function() {
+
+//   window.navigator.geolocation.getCurrentPosition( //make the browser try to find geolocalization of user (sometimes asks the permission)
+//     (position) => console.log(position), //sucess callback, it's called when the function finds a position
+//     (err) => console.log(err) //failure callback, it's called when the function can't find the user's position
+//   );
+
+//   return ( 
   
-    <div>
-      <div>Bem vindo!!!!!</div>
-      <SeasonDisplay />
-    </div>
+//     <div>
+//       <div>Bem vindo!!!!! </div>
+//       <SeasonDisplay />
+//     </div>
 
-  );
-};
+//   );
+// };
 
 ReactDOM.render(
   <App />,
